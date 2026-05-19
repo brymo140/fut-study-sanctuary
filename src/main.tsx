cf15d9f7814fd84070b3e900b5648f829ab8364d
brymo140 <88109596+brymo140@users.noreply.github.com>
2026-05-19 21:18:07 +0100

    Register service worker for PWA
    Added service worker registration for PWA support.

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,13 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+// Register PWA service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(() => {
+      console.log('[PWA] Service worker registered');
+    }).catch((err) => {
+      console.log('[PWA] Service worker failed:', err);
+    });
+  });
+}
